7ecdd16feb7c52114e5c9b7fec6056bb19ec3f43
saothienhat <saothienhat@gmail.com>
2019-07-05 14:56:34 +0700

    init project
    init project

---
+++ configserverdemo/src/main/java/com/saothienhat/configserverdemo/ConfigserverdemoApplication.java
@@ -0,0 +1,13 @@
+package com.saothienhat.configserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverdemoApplication.class, args);
+	}
+
+}
